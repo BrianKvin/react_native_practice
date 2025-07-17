b5b17e8ef71f93b5387c80ce2b06847f421688b7
briankvin <briankvin@gmail.com>
2025-07-17 07:30:41 +0300

    initial commit

---
+++ bookstore/backend/src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import "dotenv/config";
+import authRoutes from "./routes/authRoutes.js";
+
+const app = express();
+
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+})
+
